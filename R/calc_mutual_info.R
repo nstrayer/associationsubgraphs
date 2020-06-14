2ab1888141cfc0492c764a2d28f77fb9bc6d52f8
Nick Strayer <nick.strayer@gmail.com>
2020-06-14 13:16:28 -0500

    Updated the mutual information calculation function to use contingency tables for flexibility

--- R/calc_mutual_info.R
+++ R/calc_mutual_info.R
@@ -1,27 +1,43 @@
+#' Calculate mutual information from two vectors
+#'
+#' Calculates mutual information from two vectors of observations for
+#' non-continuous data. There is no parametric assumption here but if the
+#' number of unique values for either vector is high smoothing (see other
+#' package functions) may improve performance.
+#'
+#' Note that mutual information is symmetric so `calc_mutual_info(a,b) == calc_mutual_info(b,a)`.
+#'
+#' @param a_vec vector of observations (can be logical, factor, character, or integer).
+#' @param b_vec paired observations for second variable.
+#' @inheritParams calc_ent_from_table
+#'
+#' @return mutual information between the two vectors.
+#' @export
+#'
+#' @examples
+#'
+# N <- 100
+#
+# # Works with binary observation data
+# a_binom <- rbinom(N, 1, 0.5)
+# b_binom <- rbinom(N, 1, 0.8)
+# calc_mutual_info(a_binom, b_binom)
+#
+# # Also works with poisson count data (best low lambda)
+# a_pois <- rpois(N, 5)
+# b_pois <- rpois(N, 8)
+# calc_mutual_info(a_pois, b_pois)
+#'
+calc_mutual_info <- function(a_vec, b_vec, log_base = 2){
 
-calc_mutual_info <- function(a_vec, b_vec, N){
+  joint_tab <- table(a = a_vec, b = b_vec)
+  N <- length(a_vec)
 
-  ab_tab <- table(a_vec, b_vec)
+  joint_ent <- calc_ent_from_table(joint_tab, N, log_base)
+  a_ent <- calc_ent_from_table(margin.table(joint_tab, 1), N, log_base)
+  b_ent <- calc_ent_from_table(margin.table(joint_tab, 2), N, log_base)
 
-  P_A <- sum(ab_tab[2,])/N
-  P_B <- sum(ab_tab[,2])/N
-  P_notA <- 1 - P_A
-  P_notB <- 1 - P_B
-  P_notA_notB <- ab_tab[1,1]/N
-  P_notA_B <- ab_tab[1,2]/N
-  P_A_notB <- ab_tab[2,1]/N
-  P_A_B <- ab_tab[2,2]/N
-
-  ent_A <- -(P_A*e_log(P_A) +
-               P_notA*e_log(P_notA))
-
-  ent_B <- -(P_B*e_log(P_B) +
-               P_notB*e_log(P_notB))
-
-  ent_AB <- -(P_notA_notB*e_log(P_notA_notB) +
-                P_A_notB*e_log(P_A_notB) +
-                P_notA_B*e_log(P_notA_B) +
-                P_A_B*e_log(P_A_B))
-
-  ent_A + ent_B - ent_AB
+  a_ent + b_ent - joint_ent
 }
+
+
