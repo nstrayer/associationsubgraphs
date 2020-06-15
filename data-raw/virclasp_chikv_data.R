2d9fc8f873b7d211c66a8342578dcfa7e2de12cb
Nick Strayer <nick.strayer@gmail.com>
2020-06-15 16:42:30 -0500

    virclasp data ids are now cleaned up

--- data-raw/virclasp_chikv_data.R
+++ data-raw/virclasp_chikv_data.R
@@ -2,10 +2,15 @@
 
 library(tidyverse)
 library(here)
-raw <- read_tsv(here("data-raw/arcos_et_al.txt"))
 
-virclasp_chikv <- raw %>%
+virclasp_chikv <- read_tsv(here("data-raw/arcos_et_al.txt")) %>%
   select(id = `Majority protein IDs`, contains("Unique peptides ", ignore.case = FALSE)) %>%
+  filter(!str_detect(id, "REV_|CON_")) %>%
+  mutate(
+    id_clean = str_extract(id, "(?<=[a-z]{2}\\|[AOPQ][0-9][A-Z0-9]{3}[0-9]\\|).+?(?=\\;|$)"),
+    id = ifelse(is.na(id_clean), id, id_clean)
+  ) %>%
+  select(-id_clean) %>%
   pivot_longer(-id, names_to = "condition", values_to = "count") %>%
   mutate(
     condition = str_remove(condition, "Unique peptides "),
