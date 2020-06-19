6be109fd2eb86d14cb02e7e275ad541a77a655b9
Nick Strayer <nick.strayer@gmail.com>
2020-06-19 15:45:27 -0500

    Added some scratch code for dealing with virclasp data

--- data-raw/virclasp_protein_net.R
+++ data-raw/virclasp_protein_net.R
@@ -21,4 +21,26 @@ if(has_cached){
   readr::write_rds(virclasp_protein_net, cache_loc)
 }
 
+
+
+# contaminants <- "FLN|PLEC|TLN|MVP"
+#
+# virclasp_clean <- virclasp_protein_net %>%
+#   dplyr::filter(!(stringr::str_detect(a, contaminants) | stringr::str_detect(b, contaminants)))
+#
+# virclasp_strengths <- dplyr::bind_rows(
+#   dplyr::rename(virclasp_clean, id = a, target = b),
+#   dplyr::rename(virclasp_clean, id = b, target = a)
+# ) %>%
+#   dplyr::group_by(id) %>%
+#   dplyr::summarise(avg_strength = mean(strength))
+#
+# virclasp_by_strength <- virclasp_clean %>%
+#   left_join(virclasp_strengths, by = c("a" = "id")) %>%
+#   rename(a_avg = avg_strength) %>%
+#   left_join(virclasp_strengths, by = c("b" = "id")) %>%
+#   rename(b_avg = avg_strength) %>%
+#   mutate(relative_strength = strength / (a_avg + b_avg)) %>%
+#   arrange(desc(relative_strength))
+
 usethis::use_data(virclasp_protein_net, overwrite = TRUE)
