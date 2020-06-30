11489d952d2cac002500616e772e3ca44f0c6bf7
Nick Strayer <nick.strayer@gmail.com>
2020-06-30 16:08:21 -0500

    Improved code readability and added exporting of the sum of edge weights for a component

--- src/find_components.cpp
+++ src/find_components.cpp
@@ -1,29 +1,77 @@
 #include <Rcpp.h>
 
-#include <map>
 #include <list>
+#include <map>
 
 using namespace Rcpp;
+
+using Node_to_Component = std::map<String, int>;
 
 struct Component {
   std::list<String> members;
   int num_edges = 1;
-  const int num_members() const {
-    return members.size();
-  }
-  void absorb(Component& smaller_component){
+  int id = 0;
+  double total_edge_w = 0;
+  const int num_members() const { return members.size(); }
 
-    members.splice(members.end(), smaller_component.members);
-    // We always absorb when an edge is added so add it here
-    num_edges += (smaller_component.num_edges + 1);
+  void add_edge(const String& a_id,
+                const String& b_id,
+                const double& w_i,
+                Node_to_Component& node_component_ids) {
+    add_member(a_id, node_component_ids);
+    add_member(b_id, node_component_ids);
+    total_edge_w += w_i;
+  }
+
+  void add_member(const String& member_id,
+                  Node_to_Component& node_component_ids) {
+    members.push_back(member_id);
+    node_component_ids[member_id] = id;
+  }
+
+  void add_edge(const String& member_id,
+                const double& w_i,
+                Node_to_Component& node_component_ids) {
+    add_member(member_id, node_component_ids);
+    total_edge_w += w_i;
+  }
+
+  void add_edge(const double& w_i) {
+    num_edges++;
+    total_edge_w += w_i;
   }
 };
 
+inline void merge_components(Component& C_a,
+                             Component& C_b,
+                             const double& w_i,
+                             Node_to_Component& node_component_ids,
+                             std::map<int, Component>& all_components) {
+  const bool a_is_larger = C_a.num_members() > C_b.num_members();
+
+  Component& C_small = a_is_larger ? C_b : C_a;
+  Component& C_large = a_is_larger ? C_a : C_b;
+
+  // Set all members of the smaller component to have this components id
+  for (const auto& member_id : C_small.members) {
+    node_component_ids[member_id] = C_large.id;
+  }
+
+  // Merge the members lists of the two components
+  C_large.members.splice(C_large.members.end(), C_small.members);
+
+  // We always absorb when an edge is added so add it here
+  C_large.num_edges += (C_small.num_edges + 1);
+  C_large.total_edge_w += (C_small.total_edge_w + w_i);
+
+  // Tell the components map to get rid of the smaller component
+  all_components.erase(all_components.find(C_small.id));
+}
+
 // [[Rcpp::export]]
 List find_components(CharacterVector a, CharacterVector b, NumericVector w) {
-
   std::map<int, Component> components;
-  std::map<String, int> node_to_component;
+  Node_to_Component node_to_component;
 
   int component_id_counter = 0;
   const int n = a.length();
@@ -34,7 +82,6 @@ List find_components(CharacterVector a, CharacterVector b, NumericVector w) {
   NumericVector avg_size(n);
   IntegerVector max_size(n);
   NumericVector rel_max_size(n);
-  NumericVector max_density(n);
   NumericVector avg_density(n);
   NumericVector density_score(n);
   List step_component_info(n);
@@ -42,6 +89,7 @@ List find_components(CharacterVector a, CharacterVector b, NumericVector w) {
   for (int i = 0; i < n; i++) {
     const String& a_id = a[i];
     const String& b_id = b[i];
+    const double w_i = w[i];
 
     auto a_component_id_loc = node_to_component.find(a_id);
     auto b_component_id_loc = node_to_component.find(b_id);
@@ -49,9 +97,7 @@ List find_components(CharacterVector a, CharacterVector b, NumericVector w) {
     const bool a_has_component = a_component_id_loc != node_to_component.end();
     const bool b_has_component = b_component_id_loc != node_to_component.end();
 
-    bool state_has_changed = true;
-
-    if(a_has_component && b_has_component) {
+    if (a_has_component && b_has_component) {
       // Both have subgraphs
       const int a_component_id = a_component_id_loc->second;
       const int b_component_id = b_component_id_loc->second;
@@ -59,63 +105,32 @@ List find_components(CharacterVector a, CharacterVector b, NumericVector w) {
       // Are they the same subgraph?
       const bool different_component = a_component_id != b_component_id;
 
-      if(different_component){
-        Component& a_component = components[a_component_id];
-        Component& b_component = components[b_component_id];
+      if (different_component) {
+        // Merge the smaller subgraph's members into largers
+        merge_components(components[a_component_id], components[b_component_id],
+                         w_i, node_to_component, components);
 
-        if (a_component.num_members() > b_component.num_members()){
-          // Merge the smaller subgraph's members into largers
-
-          for (const auto& member_id : b_component.members) {
-            node_to_component[member_id] = a_component_id;
-          }
-
-          a_component.absorb(b_component);
-          components.erase(components.find(b_component_id));
-
-        } else {
-
-          for (const auto& member_id : a_component.members) {
-            node_to_component[member_id] = b_component_id;
-          }
-          b_component.absorb(a_component);
-          components.erase(components.find(a_component_id));
-        }
       } else {
-        state_has_changed = false;
-        components[a_component_id].num_edges++;
+        components[a_component_id].add_edge(w_i);
       }
-    } else if(!a_has_component && !b_has_component){
+    } else if (!a_has_component && !b_has_component) {
       // Neither have subgraphs
 
       // Make a new subgraph
-      const int new_component_id = component_id_counter++;
-      Component& new_component = components[new_component_id];
-      new_component.members.push_back(a_id);
-      new_component.members.push_back(b_id);
+      Component& new_component = components[++component_id_counter];
+      new_component.id = component_id_counter;
 
-      // Assign both a and b to it
-      node_to_component[a_id] = new_component_id;
-      node_to_component[b_id] = new_component_id;
+      // Set both a and b nodes to have new component
+      new_component.add_edge(a_id, b_id, w_i, node_to_component);
 
-    } else if (a_has_component){
-      // Just a has subgraph
-
-      // add b as member of a's subgraph
-      Component& a_component = components[a_component_id_loc->second];
-      a_component.members.push_back(b_id);
-      a_component.num_edges++;
-
-      node_to_component[b_id] = a_component_id_loc->second;
+    } else if (a_has_component) {
+      // If just a has subgraph add b as member of a's subgraph
+      components[a_component_id_loc->second].add_edge(b_id, w_i,
+                                                      node_to_component);
     } else {
-      // just b has a subgraph
-
-      // add a as member of b's subgraph
-      Component& b_component = components[b_component_id_loc->second];
-      b_component.members.push_back(a_id);
-      b_component.num_edges++;
-
-      node_to_component[a_id] = b_component_id_loc->second;
+      // If just b has a subgraph add a as member of b's subgraph
+      components[b_component_id_loc->second].add_edge(a_id, w_i,
+                                                      node_to_component);
     }
 
     const int nodes_seen = node_to_component.size();
@@ -124,119 +139,50 @@ List find_components(CharacterVector a, CharacterVector b, NumericVector w) {
     IntegerVector ids(num_components);
     IntegerVector sizes(num_components);
     NumericVector densities(num_components);
+    NumericVector strengths(num_components);
 
     int step_max_size = 0;
-    double step_max_density = 0;
+    double total_density = 0;
     int k = 0;
     for (const auto& component_itt : components) {
       const int Nv = component_itt.second.num_members();
-      const double dens = double(Nv)/double(component_itt.second.num_edges);
+      const double dens =
+          double(component_itt.second.num_edges) / double(Nv * (Nv - 1)) / 2.0;
       ids[k] = component_itt.first;
       sizes[k] = Nv;
       densities[k] = dens;
+      strengths[k] = component_itt.second.total_edge_w;
+      total_density += dens;
       k++;
-      if(Nv > step_max_size) step_max_size = Nv;
-      if(dens > step_max_density) step_max_density = dens;
+      if (Nv > step_max_size)
+        step_max_size = Nv;
     }
 
     n_nodes_seen[i] = nodes_seen;
     n_components[i] = num_components;
     max_size[i] = step_max_size;
-    rel_max_size[i] = double(step_max_size)/double(nodes_seen);
-    avg_size = double(nodes_seen)/double(num_components);
-    max_density[i] = step_max_density;
-    avg_density[i] = double(nodes_seen)/double(i);
-    step_component_info[i] = DataFrame::create(
-      _["size"] = sizes,
-      _["density"] = densities
-    );
+    rel_max_size[i] = double(step_max_size) / double(nodes_seen);
+    avg_size[i] = double(nodes_seen) / double(num_components);
+    avg_density[i] = total_density / double(num_components);
+    step_component_info[i] =
+        DataFrame::create(_["id"] = ids, _["size"] = sizes,
+                          _["density"] = densities, _["strength"] = strengths);
   }
 
   return List::create(
-    _["step"] = seq(1,n),
-    _["strength"] = w,
-    _["n_nodes_seen"] = n_nodes_seen,
-    _["n_components"] = n_components,
-    _["max_size"] = max_size,
-    _["rel_max_size"] = rel_max_size,
-    _["avg_size"] = avg_size,
-    _["max_density"] = max_density,
-    _["avg_density"] = avg_density,
-    _["components"] = step_component_info
-  );
+      _["n_edges"] = seq_len(n), _["strength"] = w,
+      _["n_nodes_seen"] = n_nodes_seen, _["n_components"] = n_components,
+      _["max_size"] = max_size, _["rel_max_size"] = rel_max_size,
+      _["avg_size"] = avg_size, _["avg_density"] = avg_density,
+      _["components"] = step_component_info);
 }
-
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically
-// run after the compilation.
-//
 
 /*** R
 # library(entropynet)
-data <- head(dplyr::arrange(virus_net, dplyr::desc(strength)), 1500)
-
+data <- head(dplyr::arrange(virus_net, dplyr::desc(strength)), 1000)
 res <- dplyr::as_tibble(find_components(data$a, data$b, data$strength))
 
 
-#
-# # step_stats <- res$stats
-#
-#
-#
-# step_stats <- map_dfr(res$subgraphs, function(step_df){
-#   step_df %>%
-#     mutate(density = Nv/Ne) %>%
-#     summarise(
-#       med_density = median(density),
-#       min_density = min(density),
-#       avg_density = mean(density),
-#       density_sd = sd(density),
-#       max_size = max(Nv),
-#       median_size = median(Nv),
-#       avg_size = mean(Nv)
-#     )
-# }) %>%
-#   bind_cols(res$stats) %>%
-#   mutate(
-#     step = row_number(),
-#     density_stat = avg_density*n_components,
-#     rel_max_size = max_size/n_nodes_seen
-#   )
-#
-#
-# step_stats %>%
-#   mutate(
-#     density_lower = avg_density - (1.96*density_sd)/sqrt(n_components),
-#     density_upper = avg_density + (1.96*density_sd)/sqrt(n_components)
-#   ) %>%
-#   ggplot(aes(x = step, y = avg_density)) +
-#   geom_ribbon(aes(ymin = density_lower, ymax = density_upper))
-#
-# step_stats %>%
-#   mutate(
-#     density_stat = avg_density*n_components
-#   ) %>%
-#   pivot_longer(
-#     cols = c(n_components, avg_density, density_stat)
-#   ) %>%
-#   ggplot(aes(x = step, y = value)) +
-#   geom_line() +
-#   facet_grid(rows = vars(name), scales = "free_y")
-#
-#
-# step_of_max_rel <- which(step_stats$rel_max_size == min(step_stats$rel_max_size))[1]
-# step_stats %>%
-#   pivot_longer(-step) %>%
-#   ggplot(aes(x = step, y = value)) +
-#   geom_line() +
-#   geom_vline(xintercept = step_of_max_rel, color = 'orangered') +
-#   facet_grid(rows = vars(name), scales = "free_y")
-#
-#
-# virus_net %>%
-#  dplyr::arrange(desc(strength)) %>%
-#  head(step_of_max_rel) %>%
-#  visualize_association_network()
+res$components %>% pluck(5)
 
 */
