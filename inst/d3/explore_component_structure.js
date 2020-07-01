cbb3e0bca266bb2c11abdd9dff085e160919fce6
Nick Strayer <nick.strayer@gmail.com>
2020-07-01 18:48:47 -0500

    Added beginnings of plots for components for a given step.

--- inst/d3/explore_component_structure.js
+++ inst/d3/explore_component_structure.js
@@ -19,6 +19,71 @@ const timeline_settings = {
   callout_r: 3,
 };
 
+const component_settings = {
+  rel_height: 1 / 2,
+};
+
+const components_g = g
+  .append("g")
+  .attr("width", w)
+  .attr("height", h * component_settings.rel_height);
+
+const current_components = data[50].components;
+components_g.call(draw_components, current_components);
+
+function draw_components(g, components) {
+  const h = +g.attr("height");
+  const w = +g.attr("width");
+  const sizes = {
+    size_bars: h * (2 / 5),
+    density_bars: h * (1 / 5),
+    strength_lines: h * (2 / 5),
+  };
+  const X = d3.scaleBand().domain(components.id).range([0, w]);
+  const component_w = X.bandwidth();
+
+  const sizes_Y = d3
+    .scaleLinear()
+    .domain([0, d3.max(components.size)])
+    .range([0, sizes.size_bars]);
+
+  const densities_Y = d3
+    .scaleLinear()
+    .domain([0, d3.max(components.density)])
+    .range([0, sizes.density_bars]);
+
+  const strengths_Y = d3
+    .scaleLinear()
+    .domain([0, d3.max(components.strength)])
+    .range([0, sizes.strength_lines]);
+
+  g.selectAll("g.component_stats")
+    .data(HTMLWidgets.dataframeToD3(components))
+    .enter()
+    .append("g")
+    .classed("component_stats", true)
+    .attr("transform", (d) => `translate(${X(d.id)}, 0)`)
+    .each(function (d) {
+      const component_g = d3.select(this);
+
+      component_g
+        .append("rect")
+        .attr("width", component_w)
+        .attr("y", sizes.size_bars - sizes_Y(d.size))
+        .attr("height", sizes_Y(d.size))
+        .attr("fill", "steelblue");
+
+      component_g
+        .append("circle")
+        .attr("cy", h - sizes.strength_lines + strengths_Y(d.strength))
+        .attr("cx", component_w / 2)
+        .attr("r", 5)
+        .attr("fill", "steelblue");
+    });
+}
+
+//debugger;
+
 g.call(draw_timelines, data, timeline_settings, { w, h });
 
 function draw_timelines(g, data, settings, { w, h }) {
@@ -26,10 +91,7 @@ function draw_timelines(g, data, settings, { w, h }) {
 
   const timeline_g = g
     .append("g")
-    .attr(
-      "transform",
-      `translate(0, ${h - total_h})`
-    );
+    .attr("transform", `translate(0, ${h - total_h})`);
 
   const non_metric_keys = [
     "step",
@@ -41,7 +103,6 @@ function draw_timelines(g, data, settings, { w, h }) {
   const all_metrics = Object.keys(data[0]).filter(
     (key) => !non_metric_keys.includes(key)
   );
-
 
   const chart_h = total_h / all_metrics.length;
   const chart_w = w - settings.margin_left;
@@ -96,13 +157,13 @@ function draw_timelines(g, data, settings, { w, h }) {
       draw_metric_line({ g: d3.select(this), d, settings });
     });
 
-    const callout_line = timeline_g
-      .append("line")
-      .attr("y1", settings.padding)
-      .attr("y2", total_h - settings.padding)
-      .attr("stroke", "grey")
-      .attr("stroke-opacity", 0.5)
-      .attr("stroke-width", 1);
+  const callout_line = timeline_g
+    .append("line")
+    .attr("y1", settings.padding)
+    .attr("y2", total_h - settings.padding)
+    .attr("stroke", "grey")
+    .attr("stroke-opacity", 0.5)
+    .attr("stroke-width", 1);
 
   timeline_g
     .append("rect")
@@ -114,7 +175,6 @@ function draw_timelines(g, data, settings, { w, h }) {
     .on("mousemove", on_mousemove)
     .on("mouseout", on_mouseout)
     .on("click", on_click);
-
 
   const get_step_i = (mouse_pos) => Math.round(X.invert(mouse_pos[0])) - 1;
 
@@ -133,93 +193,92 @@ function draw_timelines(g, data, settings, { w, h }) {
     const step_i = get_step_i(d3.mouse(this));
     console.log(`Selected step ${step_i}`);
   }
-}
+  function draw_metric_line({ g, d, settings }) {
+    const { X, Y } = d;
+    const {
+      background_color,
+      background_alpha,
+      line_color,
+      line_width,
+      padding,
+      callout_r,
+    } = settings;
+    const chart_hight = +g.attr("height");
+    const chart_width = +g.attr("width");
 
-function draw_metric_line({ g, d, settings }) {
-  const { X, Y } = d;
-  const {
-    background_color,
-    background_alpha,
-    line_color,
-    line_width,
-    padding,
-    callout_r,
-  } = settings;
-  const chart_hight = +g.attr("height");
-  const chart_width = +g.attr("width");
+    g.append("rect")
+      .attr("width", chart_width)
+      .attr("height", chart_hight - 2 * padding)
+      .attr("y", padding)
+      .attr("fill", background_color)
+      .attr("fill-opacity", background_alpha);
 
-  g.append("rect")
-    .attr("width", chart_width)
-    .attr("height", chart_hight - 2 * padding)
-    .attr("y", padding)
-    .attr("fill", background_color)
-    .attr("fill-opacity", background_alpha);
+    g.append("text")
+      // .attr("text-anchor", "end")
+      .attr("x", chart_width + 4)
+      .attr("y", chart_hight / 2)
+      .attr("dominant-baseline", "middle")
+      .text(d.id.replace(/_/g, " "));
 
-  g.append("text")
-    // .attr("text-anchor", "end")
-    .attr("x", chart_width + 4 )
-    .attr("y", chart_hight / 2)
-    .attr("dominant-baseline", "middle")
-    .text(d.id.replace(/_/g, " "));
+    g.append("g")
+      .call(d3.axisLeft(Y).ticks(2))
+      .call(extend_ticks, chart_width)
+      .call(remove_domain);
 
-  g.append("g")
-    .call(d3.axisLeft(Y).ticks(2))
-    .call(extend_ticks, chart_width)
-    .call(remove_domain);
+    g.append("path")
+      .attr("d", d.path)
+      .attr("fill", "none")
+      .attr("stroke-width", line_width)
+      .attr("stroke", line_color);
 
-  g.append("path")
-    .attr("d", d.path)
-    .attr("fill", "none")
-    .attr("stroke-width", line_width)
-    .attr("stroke", line_color);
+    // A hidden callout for values revealed on mouseover
+    const callout = g.append("g").attr("id", `${d.id}-callout`);
 
-  // A hidden callout for values revealed on mouseover
-  const callout = g.append("g").attr("id", `${d.id}-callout`);
-
-  const callout_background = callout
-    .append("rect")
-    .attr("fill", "white")
-    .attr("rx", 5)
-    .attr("ry", 5)
-    .attr("filter", "url(#blur_filter)");
-
-  callout
-    .append("circle")
-    .attr("r", callout_r)
-    .attr("fill", "none")
-    .attr("stroke", "black")
-    .attr("stroke-width", 1);
-
-  const callout_text = callout
-    .append("text")
-    .attr("x", callout_r)
-    .attr("y", callout_r);
-
-  d.set_callout = function (step_i) {
-    const x_pos = X(step_i);
-    const past_halfway = x_pos > w / 2;
-    const value_at_step = d.values[step_i];
+    const callout_background = callout
+      .append("rect")
+      .attr("fill", "white")
+      .attr("rx", 5)
+      .attr("ry", 5)
+      .attr("filter", "url(#blur_filter)");
 
     callout
-      .attr("visibility", "visible")
-      .attr("transform", `translate(${x_pos}, ${Y(value_at_step)})`);
+      .append("circle")
+      .attr("r", callout_r)
+      .attr("fill", "none")
+      .attr("stroke", "black")
+      .attr("stroke-width", 1);
 
-    callout_text
-      .text(d3.format(d.is_integer ? ",i" : ".3f")(d.values[step_i]))
-      .attr("text-anchor", past_halfway ? "end" : "start")
-      .attr("x", past_halfway ? -10 : 10);
+    const callout_text = callout
+      .append("text")
+      .attr("x", callout_r)
+      .attr("y", callout_r);
 
-    const text_bbox = callout_text.node().getBBox();
-    const pad = 3;
-    callout_background
-      .attr("width", text_bbox.width + pad * 2)
-      .attr("height", text_bbox.height)
-      .attr("x", text_bbox.x - pad)
-      .attr("y", text_bbox.y);
-  };
+    d.set_callout = function (step_i) {
+      const x_pos = X(step_i);
+      const past_halfway = x_pos > w / 2;
+      const value_at_step = d.values[step_i];
 
-  d.hide_callout = function () {
-    callout.attr("visibility", "hidden");
-  };
-  d.hide_callout();
+      callout
+        .attr("visibility", "visible")
+        .attr("transform", `translate(${x_pos}, ${Y(value_at_step)})`);
+
+      callout_text
+        .text(d3.format(d.is_integer ? ",i" : ".3f")(d.values[step_i]))
+        .attr("text-anchor", past_halfway ? "end" : "start")
+        .attr("x", past_halfway ? -10 : 10);
+
+      const text_bbox = callout_text.node().getBBox();
+      const pad = 3;
+      callout_background
+        .attr("width", text_bbox.width + pad * 2)
+        .attr("height", text_bbox.height)
+        .attr("x", text_bbox.x - pad)
+        .attr("y", text_bbox.y);
+    };
+
+    d.hide_callout = function () {
+      callout.attr("visibility", "hidden");
+    };
+    d.hide_callout();
+  }
 }
