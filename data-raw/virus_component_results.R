748c8275113d9615d9ce2fd2d21a374832a7584e
Nick Strayer <nick.strayer@gmail.com>
2020-07-02 14:27:07 -0500

    Regenerated virus data with the new proper density metrics

--- data-raw/virus_component_results.R
+++ data-raw/virus_component_results.R
@@ -5,7 +5,7 @@ library(entropynet)
 association_pairs <- virus_net %>%
   arrange(desc(strength))
 
-virus_component_results <- association_pairs %>% head(5000) %>% explore_component_structure()
+virus_component_results <- association_pairs %>% head(5000) %>% find_components(w_col = "strength") %>% as_tibble()
 
 
 usethis::use_data(virus_component_results, overwrite = TRUE)
