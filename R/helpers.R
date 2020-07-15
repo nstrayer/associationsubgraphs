0c83df6db8708a6becb6efd922a9d11f040b10c4
Nick Strayer <nick.strayer@gmail.com>
2020-07-15 18:51:09 -0500

    Forgot to add a default value for strength column

--- R/helpers.R
+++ R/helpers.R
@@ -8,7 +8,7 @@ gather_unique_nodes <- function(pairs){
 }
 
 # Lots of functions in package rely on association network being sorted
-ensure_sorted <- function(association_pairs, strength_column){
+ensure_sorted <- function(association_pairs, strength_column = "strength"){
   desc_strengths <- -association_pairs[[strength_column]]
   if(is.unsorted(desc_strengths)){
     return(association_pairs[order(desc_strengths),])
